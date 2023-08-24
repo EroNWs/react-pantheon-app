aebb5d916baa9cfca3601ca3763025eb26322101
mehmeterengormez <merengrmz@hotmail.com>
2023-08-24 22:01:13 +0300

    theme

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-
+//s
 export const ThemeContext = createContext({
   theme: 'light', // varsayılan tema
   toggleTheme: () => {} // temayı değiştirmek için boş bir fonksiyon
