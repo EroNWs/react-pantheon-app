3b2bc5b6ecbbbeedcad96c4f37c4cda0419e7e85
mehmeterengormez <merengrmz@hotmail.com>
2023-08-25 00:28:18 +0300

    switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // Import React and hooks
 import './App.css';
 import { ThemeContext } from './ThemeContext';
-import Home from './Home';
-import LoginForm from './LoginForm';
-import SignUpForm from './SignUpForm';
-import BuildingConfiguration from './BuildingConfiguration';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'; // Import Router components
+import Home from './Home'; // Import Home component
+import LoginForm from './LoginForm'; // Import LoginForm component
+import SignUpForm from './SignUpForm'; // Import SignUpForm component
+import BuildingConfiguration from './BuildingConfiguration'; // Import BuildingConfiguration component
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import Router, Route, and Switch
 
 function App() {
     const [theme, setTheme] = useState('light'); 
@@ -25,20 +25,30 @@ function App() {
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            <Router>
+            <Router> {/* Wrap the app with Router */}
                 <div className={`App ${theme}`}>
+                    {/* Render the components based on the route */}
                     <Switch>
+                        <Route exact path="/">
+                            <Home 
+                                isLoggedIn={isLoggedIn}
+                                setIsLoggedIn={setIsLoggedIn}
+                            />
+                        </Route>
                         <Route path="/login">
-                            {isLoggedIn ? <Redirect to="/building-configuration" /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+                            <LoginForm 
+                                isLoggedIn={isLoggedIn}
+                                setIsLoggedIn={setIsLoggedIn}
+                            />
                         </Route>
                         <Route path="/sign-up">
-                            {isLoggedIn ? <Redirect to="/building-configuration" /> : <SignUpForm />}
+                            <SignUpForm 
+                                isLoggedIn={isLoggedIn}
+                                setIsLoggedIn={setIsLoggedIn}
+                            />
                         </Route>
                         <Route path="/building-configuration">
-                            {isLoggedIn ? <BuildingConfiguration /> : <Redirect to="/login" />}
-                        </Route>
-                        <Route path="/">
-                            <Home setIsLoggedIn={setIsLoggedIn} />
+                            {isLoggedIn ? <BuildingConfiguration /> : <Redirect to="/" />}
                         </Route>
                     </Switch>
                 </div>
