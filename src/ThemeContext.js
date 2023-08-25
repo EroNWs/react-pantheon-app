cfa6a8e0d1e6da1f15336bcbdc52e3b61d8f7cd3
mehmeterengormez <merengrmz@hotmail.com>
2023-08-25 14:14:44 +0300

    themdefs

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 
 export const ThemeContext = createContext({
-  theme: light, // default theme
+  theme: 'light', // default theme
   toggleTheme: () => {} // function for change theme 
 });
